426288490644fd31e4efd4e98df2ed597776ca56
bpitchfo <bpitchfo@gmail.com>
2018-08-28 16:01:49 -0400

    Initial Commit for Project
    Code and Files

---
+++ js/locations.js
@@ -0,0 +1,51 @@
+//Locations to be passed via the Location function which will display as a marker and look up via forusquare API
+var initialLocations = [
+	{
+		name: 'Lenox',
+		lat: 33.846536,
+        long: -84.361811,
+        address:'3393 Peachtree Rd NE Atlanta, GA 30326'
+	},
+	{
+		name: 'W Hotel',
+		lat: 33.847828,
+		long: -84.366006,
+        address:'3377 Peachtree Rd NE Atlanta, GA 30326'
+    },
+    {
+		name: 'Petsmart',
+		lat: 33.841056,
+		long: -84.372454,
+        address:'3221 Peachtree Rd NE Atlanta, GA 30305'
+	},
+	{
+		name: 'Mens Warehouse',
+		lat: 33.843507,
+		long: -84.369783,
+        address:'3255 Peachtree Rd NE Suite Aa Atlanta, GA 30305'
+    },
+    {
+		name: 'Ruths Steakhouse',
+		lat: 33.844821,
+		long: -84.368933,
+        address:'3285 Peachtree Rd NE Atlanta, GA 30305'
+	},
+	{
+		name: 'AMC Dine IN Movies',
+		lat: 33.847472,
+		long: -84.371009,
+        address:'3340 Peachtree Rd NE Atlanta, GA 30326'
+	},
+    {
+		name: 'Phips Plaza',
+		lat: 33.852646,
+		long: -84.361771,
+        address: '3500 Peachtree Rd NE Atlanta, GA 30326'
+    },
+    {
+		name: 'JW Marriot',
+		lat: 33.845594,
+		long: -84.360323,
+        address:'3300 Lenox Rd NE Atlanta, GA 30326'
+	}
+];
